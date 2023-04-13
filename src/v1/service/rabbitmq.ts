d939c325cc8dc9ead55a5eb97a66db62405fbac0
Julia <denadaijulia@gmail.com>
2023-04-12 23:18:44 -0300

    add rabbitmq connection

---
+++ src/v1/service/rabbitmq.ts
@@ -0,0 +1,31 @@
+import { Channel, connect } from 'amqplib';
+import { config } from 'dotenv';
+
+var amqp = require('amqplib/callback_api');
+
+class RabbitMQ{
+
+  async createMessageChannel(): Promise<Channel | null> {
+    config()
+
+    try {
+      console.log(process.env.AMQP_SERVER)
+      const connection = await connect(process.env.AMQP_SERVER ?? "")
+
+      const channel = await connection.createChannel()
+      await channel.assertQueue(process.env.QUEUE_NAME ?? "default")
+
+      console.log(channel)
+
+      return channel
+    }
+    catch(error){
+
+      console.log(error)
+      return null
+    }
+  }
+  
+}
+
+export { RabbitMQ }
