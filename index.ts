5d4c4586fd8014b7d76fe35e98754771d57ed1d2
JuliaDeNadai <denadaijulia@gmail.com>
2024-01-15 16:23:05 -0300

    remove: test

--- index.ts
+++ index.ts
@@ -4,8 +4,6 @@ var http = app.get('http');
 
 const port = /* process.env.PORT || */ 3333;
 
-console.log('teste')
-
 http.listen(port, () => {
   console.log(`Servidor iniciado, porta ${port}... ☑`);
 });
